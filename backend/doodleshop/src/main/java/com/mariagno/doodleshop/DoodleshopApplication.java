56cdd0281923872bcadf8123a764f0bdd2e30552
markariagno22 <Mark.ariagno14@gmail.com>
2025-10-02 17:36:30 -0500

    Initial commit: frontend and backend setup

---
+++ backend/doodleshop/src/main/java/com/mariagno/doodleshop/DoodleshopApplication.java
@@ -0,0 +1,13 @@
+package com.mariagno.doodleshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoodleshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoodleshopApplication.class, args);
+	}
+
+}
---
+++ backend/doodleshop/src/test/java/com/mariagno/doodleshop/DoodleshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mariagno.doodleshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoodleshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
